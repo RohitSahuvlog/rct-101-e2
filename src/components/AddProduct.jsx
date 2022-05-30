b2a81b5e1423da6e8a4513d929273f8bb262d527
Rohit sahu <96680038+RohitSahuvlog@users.noreply.github.com>
2022-05-30 17:41:05 +0530

    master

--- src/components/AddProduct.jsx
+++ src/components/AddProduct.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 
-const AddProduct = () => {
+const AddProduct = ({postData}) => {
+
   // TODO: Remove below const and instead import them from chakra
   const Button = () => <div />;
   const Modal = () => <div />;
@@ -10,24 +11,52 @@ const AddProduct = () => {
   const RadioGroup = () => <div />;
   const Radio = () => <div />;
 
+const [inp,setinp]=useState({})
+const handleform=()=>{
+  // e.preventDefalut()
+  // let res = await fetch ("http://localhost:8080/products",{
+  //   method:"POST",
+  //   header:{"context-type":"application/json"},
+  //   body:JSON.stringify(inp)
+      
+  //   } )
+  //   let data =await res.json()
+postData([...setinp])
+
+}
+const handlechang=(e)=>{
+var nameval =e.target.value
+if(e.target.type==="checkbox"){
+setinp({
+  ...inp,
+  [nameval]:e.target.checked
+})
+}
+else{
+  setinp({
+    ...inp,
+    [nameval]:e.target.value
+  })
+}
+}
   return (
     <>
-      <Button my={4} data-cy="add-product-button"></Button>
-      <Modal>
+      <Button my={4} data-cy="add-product-button"> AddProduct</Button>
+      <Modal >
         <ModalBody pb={6}>
-          <Input data-cy="add-product-title" />
-          <Select data-cy="add-product-category">
-            <option data-cy="add-product-category-shirt"></option>
-            <option data-cy="add-product-category-pant"></option>
-            <option data-cy="add-product-category-jeans"></option>
+          <Input data-cy="add-product-title" type="text" onChange={handlechang} name="textt"/>
+          <Select data-cy="add-product-category" name="" onClick={handlechang}>
+            <option data-cy="add-product-category-shirt" >shirt</option>
+            <option data-cy="add-product-category-pant" >pant</option>
+            <option data-cy="add-product-category-jeans" >jeans</option>
           </Select>
-          <RadioGroup data-cy="add-product-gender">
-            <Radio data-cy="add-product-gender-male"></Radio>
-            <Radio data-cy="add-product-gender-female"></Radio>
-            <Radio data-cy="add-product-gender-unisex"></Radio>
+          <RadioGroup data-cy="add-product-gender" onClick={handlechang}>
+            <Radio data-cy="add-product-gender-male" >male</Radio>
+            <Radio data-cy="add-product-gender-female" >female</Radio>
+            <Radio data-cy="add-product-gender-unisex" >unisex</Radio>
           </RadioGroup>
-          <Input data-cy="add-product-price" />
-          <Button data-cy="add-product-submit-button"></Button>
+          <Input data-cy="add-product-price" onChange={handlechang} name="price" />
+          <Button data-cy="add-product-submit-button" onClick={handleform} >Create</Button>
         </ModalBody>
       </Modal>
     </>
